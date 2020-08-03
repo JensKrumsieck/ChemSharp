9cb3a76b45d8931b575603930ca86115740bd1bf
Jens Krumsieck <jenso666@live.de>
2020-08-03 16:37:07 +0200

    add simple uvvis test

---
+++ ChemSharp.Tests/VarianUVVisTest.cs
@@ -0,0 +1,26 @@
+﻿using ChemSharp.Files;
+using ChemSharp.Spectrum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChemSharp.Tests
+{
+    [TestClass]
+    public class VarianUVVisTest
+    {
+        private string path = "files/uvvis.dsw";
+
+        [TestMethod]
+        public void TestDSW()
+        {
+            var dsw = new DSW(path);
+            Assert.AreEqual(901,dsw.XYData.Length);
+        }
+
+        [TestMethod]
+        public void TestUVVis()
+        {
+            var uvvis = SpectrumFactory.Create<UVVisSpectrum, DSW>(path);
+            Assert.AreEqual(901, uvvis.Data.Length);
+        }
+    }
+}

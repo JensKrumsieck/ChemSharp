b68e2f5276d7ef72ae107b619ad67476a765a87c
Jens Krumsieck <jenso666@live.de>
2020-08-03 16:17:35 +0200

    empty demo app

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -8,13 +8,7 @@ namespace DemoApp
     {
         static void Main(string[] args)
         {
-            var uv = SpectrumFactory.Create<UVVisSpectrum, DSW>(
-                @"C:\Users\jenso\PowerFolders\Forschung\UVVis\JK001\JK001F01.DSW");
-            var path = @"C:\Users\jenso\PowerFolders\Forschung\EPR\DipyOMeIPCoCl\8K dcm\Spectrum33";
-            var epr = SpectrumFactory.Create<EPRSpectrum, PAR, SPC>($"{path}.par", $"{path}.spc");
-            
-
-            Console.WriteLine("");
+           
         }
     }
 }

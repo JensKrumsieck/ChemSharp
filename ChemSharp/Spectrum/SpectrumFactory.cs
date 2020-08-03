22a98d6944d12e64b59df8d7cd10b09be25b1ff5
Jens Krumsieck <jenso666@live.de>
2020-08-03 15:52:42 +0200

    add fast-create method for two source files

--- ChemSharp/Spectrum/SpectrumFactory.cs
+++ ChemSharp/Spectrum/SpectrumFactory.cs
@@ -9,17 +9,19 @@ namespace ChemSharp.Spectrum
     /// </summary>
     public static class SpectrumFactory
     {
-        public static AbstractSpectrum CreateFromFile<T>(IXYSpectrumFile src) where T : AbstractSpectrum
+        public static AbstractSpectrum CreateFromFile<T>(IXYSpectrumFile src)
+            where T : AbstractSpectrum
         {
-            var spc = (T) Activator.CreateInstance(typeof(T));
+            var spc = (T)Activator.CreateInstance(typeof(T));
             spc.Data = src.XYData;
             return spc;
         }
 
-        public static AbstractSpectrum CreateFromFiles<T>(IXSpectrumFile xSrc, IYSpectrumFile ySrc) where T : AbstractSpectrum
+        public static AbstractSpectrum CreateFromFiles<T>(IXSpectrumFile xSrc, IYSpectrumFile ySrc)
+            where T : AbstractSpectrum
         {
             Vector2[] data = new Vector2[xSrc.XData.Length];
-            for (int i = 0; i < xSrc.XData.Length; i++)
+            for (var i = 0; i < xSrc.XData.Length; i++)
             {
                 data[i] = new Vector2(xSrc.XData[i], ySrc.YData[i]);
             }
@@ -28,10 +30,22 @@ namespace ChemSharp.Spectrum
             return spc;
         }
 
-        public static TResult Create<TResult, TSource>(string path) where TResult : AbstractSpectrum where TSource : IXYSpectrumFile
+        public static TResult Create<TResult, TSource>(string path)
+            where TResult : AbstractSpectrum
+            where TSource : IXYSpectrumFile
         {
             var file = (TSource)Activator.CreateInstance(typeof(TSource), path);
             return (TResult)CreateFromFile<TResult>(file);
+        }
+
+        public static TResult Create<TResult, TSourceX, TSourceY>(string xPath, string yPath)
+            where TResult : AbstractSpectrum
+            where TSourceX : IXSpectrumFile
+            where TSourceY : IYSpectrumFile
+        {
+            var xFile = (TSourceX)Activator.CreateInstance(typeof(TSourceX), xPath);
+            var yFile = (TSourceY)Activator.CreateInstance(typeof(TSourceY), yPath);
+            return (TResult)CreateFromFiles<TResult>(xFile, yFile);
         }
     }
 }

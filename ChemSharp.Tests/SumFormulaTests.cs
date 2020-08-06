b6df9839415faf6f037c305547aa480b5a4ecda5
Jens Krumsieck <jenso666@live.de>
2020-08-06 14:15:02 +0200

    Add Conversion Method for Sumformula Strings

---
+++ ChemSharp.Tests/SumFormulaTests.cs
@@ -0,0 +1,41 @@
+﻿using ChemSharp.Molecule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ChemSharp.Tests
+{
+    [TestClass]
+    public class SumFormulaTests
+    {
+        [TestMethod]
+        public void TestFerrozin()
+        {
+            var formula = "K4[Fe(CN)6]";
+            var elements = formula.ToElements();
+            Assert.AreEqual(17, elements.Count());
+            var parsed = elements.SumFormula();
+            Assert.AreEqual("C6FeK4N6", parsed);
+        }
+
+        [TestMethod]
+        public void TestPorphine()
+        {
+            var formula = "C20H14N4";
+            var elements = formula.ToElements();
+            Assert.AreEqual(38, elements.Count());
+            var parsed = elements.SumFormula();
+            Assert.AreEqual("C20H14N4", parsed);
+        }
+
+        [TestMethod]
+        public void TestCarboraneAcid()
+        {
+            var formula = "H(CHB11F11)";
+            var elements = formula.ToElements();
+            Assert.AreEqual(25, elements.Count());
+            var parsed = elements.SumFormula();
+            Assert.AreEqual("B11CF11H2", parsed);
+        }
+
+    }
+}
--- ChemSharp/Molecule/Extensions.cs
+++ ChemSharp/Molecule/Extensions.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ChemSharp.Molecule
 {
     public static class Extensions
     {
+        private static string Pattern = @"([A-Z][a-z]*)(\d*[,.]?\d*)|(\()|(\))(\d*[,.]?\d*)";
 
         /// <summary>
         /// Converts List of Elements to Sum Formula
@@ -16,10 +17,10 @@ namespace ChemSharp.Molecule
         public static string SumFormula(this IEnumerable<Element> input)
         {
             var groups = from element in input
-                group element by element.Symbol
+                         group element by element.Symbol
                 into newGroup
-                orderby newGroup.Key
-                select newGroup;
+                         orderby newGroup.Key
+                         select newGroup;
             var formula = "";
             foreach (var group in groups)
             {
@@ -38,6 +39,47 @@ namespace ChemSharp.Molecule
         public static double Weight(this IEnumerable<Element> input)
         {
             return input.Sum(s => s.AtomicWeight);
+        }
+
+        public static IEnumerable<Element> ToElements(this string input)
+        {
+            input = input.Replace("[", "(".Replace("]", ")"));
+            List<List<Element>> result = new List<List<Element>>()
+            {
+                new List<Element>()
+            };
+            var index = 0;
+            foreach (Match m in Regex.Matches(input, Pattern))
+            {
+                if (m.Groups[1].Success)
+                {
+                    var amount = m.Groups[2].Success && m.Groups[2].Value != "" ? Convert.ToInt32(m.Groups[2].Value) : 1;
+                    for (var i = 0; i < amount; i++)result[index].Add(new Element(m.Groups[1].Value));
+                }
+                if (m.Groups[3].Success)
+                {
+                    index++;
+                    result.Add(new List<Element>());
+                }
+                if (m.Groups[4].Success)
+                {
+                    var multiplicity = m.Groups[5].Success && m.Groups[5].Value != ""
+                        ? Convert.ToInt32(m.Groups[5].Value)
+                        : 1;
+                    for (var j = index; j < result.Count; j++) result[j].Factor(multiplicity);
+                }
+            }
+
+            var ret = new List<Element>();
+            foreach(var list in result) ret.AddRange(list);
+            return ret;
+        }
+
+        private static List<Element> Factor(this List<Element> input, int multiplicity)
+        {
+            var tmp = new List<Element>(input);
+            for (int i = 0; i < multiplicity - 1; i++) input.AddRange(tmp);
+            return input;
         }
     }
 }

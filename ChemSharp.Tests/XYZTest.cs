ec49c8185926a9e43bd2f4a4a9d0dbf237e89b43
Jens Krumsieck <jenso666@live.de>
2020-08-06 11:36:57 +0200

    Extension Methods to calculate SumFormula and Weight

--- ChemSharp.Tests/XYZTest.cs
+++ ChemSharp.Tests/XYZTest.cs
@@ -1,6 +1,7 @@
 ﻿using ChemSharp.Files.Molecule;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using ChemSharp.Molecule;
 
 namespace ChemSharp.Tests
 {
@@ -15,6 +16,8 @@ namespace ChemSharp.Tests
         {
             var file = new XYZ(path);
             Assert.AreEqual(23, file.Atoms.Count());
+            Assert.AreEqual("C10H12O", file.Atoms.SumFormula());
+            Assert.AreEqual(148.205, file.Atoms.Weight(), 0.025);
         }
     }
 }
---
+++ ChemSharp/Molecule/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChemSharp.Molecule
+{
+    public static class Extensions
+    {
+
+        /// <summary>
+        /// Converts List of Elements to Sum Formula
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string SumFormula(this IEnumerable<Element> input)
+        {
+            var groups = from element in input
+                group element by element.Symbol
+                into newGroup
+                orderby newGroup.Key
+                select newGroup;
+            var formula = "";
+            foreach (var group in groups)
+            {
+                var count = group.Count();
+                formula += $"{group.Key}{(count != 1 ? count.ToString() : "")}";
+            }
+
+            return formula;
+        }
+
+        public static double Weight(this IEnumerable<Element> input)
+        {
+            return input.Sum(s => s.AtomicWeight);
+        }
+    }
+}

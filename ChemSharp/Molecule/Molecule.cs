9d79857a41247b6b12234f496bdeeda16eda900c
Jens Krumsieck <jenso666@live.de>
2020-08-07 10:12:50 +0200

    increase system.text,json version and got back to netstandard2.0 also added Molecule Class

---
+++ ChemSharp/Molecule/Molecule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ChemSharp.Molecule
+{
+    public class Molecule
+    {
+        public IEnumerable<Atom> Atoms { get; set; }
+
+        public Molecule(IEnumerable<Atom> atoms)
+        {
+            Atoms = atoms;
+            //as the atom only constructor is used, generate bond information
+            GenerateBonds();
+        }
+
+        public void GenerateBonds()
+        {
+            //TODO!
+        }
+    }
+}

f3852c71be5dafd099a38f0f070ab2ab1b6b33ef
Jens Krumsieck <jenso666@live.de>
2020-09-01 13:02:20 +0200

    .

--- ChemSharp/Extensions/CollectionsUtil.cs
+++ ChemSharp/Extensions/CollectionsUtil.cs
@@ -4,6 +4,14 @@ namespace ChemSharp.Extensions
 {
     public static class CollectionsUtil
     {
+        /// <summary>
+        /// shorthand method for try and get dictionary value
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static TValue TryAndGet<TKey, TValue>(this Dictionary<TKey, TValue> input, TKey key) =>
             input.ContainsKey(key) ? input[key] : default;
     }

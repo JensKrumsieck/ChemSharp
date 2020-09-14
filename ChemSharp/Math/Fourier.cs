1c6e74a4685e49f5140ad1b1b743d074500b4ed1
Jens Krumsieck <jenso666@live.de>
2020-09-14 14:18:07 +0200

    Use MathNet Numerics as being more stable than a scratch approach

--- ChemSharp/Math/Fourier.cs
+++ ChemSharp/Math/Fourier.cs
@@ -19,7 +19,7 @@ namespace ChemSharp.Math
         }
 
         /// <summary>
-        /// Radix 2 FFT Algorithm (inspired by Math.NET implementation)
+        /// For compatibility with TopSpins Fourier Transformation force RadixFFT
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
@@ -32,59 +32,8 @@ namespace ChemSharp.Math
                 x = paddingSeq.Concat(x).Concat(paddingSeq).ToArray();
             }
 
-            Radix2Reorder(x);
-            for (var levelSize = 1; levelSize < x.Length; levelSize *= 2)
-                for (var k = 0; k < levelSize; k++)
-                    Radix2Step(x, -1, levelSize, k);
-            return FullRescale(x);
-        }
-
-        private static void Radix2Step(IList<Complex> x, int exponentSign, int levelSize, int k)
-        {
-            var exponent = (exponentSign * k) * System.Math.PI / levelSize;
-            var w = new Complex(System.Math.Cos(exponent), System.Math.Sin(exponent));
-            var step = levelSize << 1;
-            for (var i = k; i < x.Count; i += step)
-            {
-                var ai = x[i];
-                var t = w * x[i + levelSize];
-                x[i] = ai + t;
-                x[i + levelSize] = ai - t;
-            }
-        }
-
-        private static void Radix2Reorder<T>(IList<T> input)
-        {
-            var j = 0;
-            for (var i = 0; i < input.Count - 1; i++)
-            {
-                if (i < j)
-                {
-                    var temp = input[i];
-                    input[i] = input[j];
-                    input[j] = temp;
-                }
-
-                var m = input.Count;
-                do
-                {
-                    m >>= 1;
-                    j ^= m;
-                } while ((j & m) == 0);
-            }
-        }
-
-
-        /// <summary>
-        /// Rescales Fourier Data
-        /// </summary>
-        /// <param name="samples"></param>
-        /// <returns></returns>
-        private static Complex[] FullRescale(Complex[] samples)
-        {
-            var scalingFactor = 1.0 / samples.Length;
-            for (var i = 0; i < samples.Length; i++) samples[i] *= scalingFactor;
-            return samples;
+            MathNet.Numerics.IntegralTransforms.Fourier.Forward(x);
+            return x;
         }
     }
 }

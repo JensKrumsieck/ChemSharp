bdf795c87290d9cf6df8e771d96ab10ec263872a
Jens Krumsieck <jenso666@live.de>
2020-12-22 15:37:57 +0100

    Refactor Read Method

--- ChemSharp.Molecules/DataProviders/Mol2DataProvider.cs
+++ ChemSharp.Molecules/DataProviders/Mol2DataProvider.cs
@@ -45,29 +45,43 @@ namespace ChemSharp.Molecules.DataProviders
             {
                 if (string.IsNullOrEmpty(line) || (line == "ATOM" || line == "BOND")) continue;
                 var cols = line.WhiteSpaceSplit();
-                if (typeof(T) == typeof(Atom))
-                {
-                    var id = cols[1];
-                    var loc = new Vector3(
-                        cols[2].ToSingle(),
-                        cols[3].ToSingle(),
-                        cols[4].ToSingle()
-                        );
-                    var sym = Regex.Match(id, @"[A-Za-z]*").Value;
-                    yield return new Atom(sym)
-                    {
-                        Title = id,
-                        Location = loc
-                    } as T;
-                }
-                else
-                {
-                    //subtract 1 as mol2 starts counting at 1
-                    var a1 = cols[1].ToInt() - 1;
-                    var a2 = cols[2].ToInt() - 1;
-                    yield return new Bond(Atoms.ElementAt(a1), Atoms.ElementAt(a2)) as T;
-                }
+                if (typeof(T) == typeof(Atom)) yield return ReadAtom(cols) as T;
+                else yield return ReadBond(cols) as T;
             }
+        }
+
+        /// <summary>
+        /// Reads Atom
+        /// </summary>
+        /// <param name="cols"></param>
+        /// <returns></returns>
+        private Atom ReadAtom(string[] cols)
+        {
+            var id = cols[1];
+            var loc = new Vector3(
+                cols[2].ToSingle(),
+                cols[3].ToSingle(),
+                cols[4].ToSingle()
+            );
+            var sym = Regex.Match(id, @"[A-Za-z]*").Value;
+            return new Atom(sym)
+            {
+                Title = id,
+                Location = loc
+            };
+        }
+
+        /// <summary>
+        /// Reads Bond
+        /// </summary>
+        /// <param name="cols"></param>
+        /// <returns></returns>
+        private Bond ReadBond(string[] cols)
+        {
+            //subtract 1 as mol2 starts counting at 1
+            var a1 = cols[1].ToInt() - 1;
+            var a2 = cols[2].ToInt() - 1;
+            return new Bond(Atoms.ElementAt(a1), Atoms.ElementAt(a2));
         }
 
 

84f34571fd5aad0b858f5d2f76163d441fceceea
Jens Krumsieck <jenso666@live.de>
2021-05-19 16:37:35 +0200

    [Feature POVRay Parsing] write test for atom parsing

--- ChemSharp.Tests/Rendering/PovRayTests.cs
+++ ChemSharp.Tests/Rendering/PovRayTests.cs
@@ -1,8 +1,11 @@
-﻿using ChemSharp.Rendering.Extensions;
+﻿using ChemSharp.Molecules;
+using ChemSharp.Rendering.Extensions;
 using ChemSharp.Rendering.Primitives;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 
 namespace ChemSharp.Tests.Rendering
@@ -53,7 +56,8 @@ namespace ChemSharp.Tests.Rendering
             {
                 Location = new Vector3(7, 7, 7),
                 LookAt = Vector3.Zero
-            }; var l = new Light
+            };
+            var l = new Light
             {
                 Location = new Vector3(5, 5, 5),
                 Color = new Vector3(1, 1, 1)
@@ -62,6 +66,38 @@ namespace ChemSharp.Tests.Rendering
 
             var pov = string.Join(Environment.NewLine, c.ToPovString(), l.ToPovString(), s.ToPovString());
             File.WriteAllText("test.pov", pov);
+        }
+
+        [TestMethod]
+        public void ParseMoleculeScene()
+        {
+            var m = MoleculeFactory.Create("files/cif.cif");
+            var c = new Camera
+            {
+                Location = new Vector3(10, 7, 40),
+                LookAt = m.Atoms.FirstOrDefault(s => s.IsMetal)?.Location ?? Vector3.Zero,
+                FieldOfView = 60
+            };
+            var l = new Light
+            {
+                Location = new Vector3(15, 5, 5),
+                Color = new Vector3(1, 1, 1)
+            };
+            var atoms = m.Atoms.Select(a =>
+                    new Sphere { Location = a.Location, Radius = (a.CovalentRadius ?? 100) / 200f, Color = ToVec3(a.Color) }
+                        .ToPovString())
+                .ToList();
+            Assert.AreEqual(atoms.Count, m.Atoms.Count);
+            var a = string.Join(Environment.NewLine, atoms);
+            var pov = "background {rgb < 0.1, 0.1, 0.1>}" + //hardcode background to gray
+                      string.Join(Environment.NewLine, c.ToPovString(), l.ToPovString(), a);
+            File.WriteAllText("mol.pov", pov);
+        }
+
+        public static Vector3 ToVec3(string hex)
+        {
+            var col = ColorTranslator.FromHtml(hex);
+            return new Vector3(col.R / 255f, col.G / 255f, col.B / 255f);
         }
     }
 }

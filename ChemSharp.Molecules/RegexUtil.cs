4c7211c215f8c05eabb903e1280a428ad1003d88
Jens Krumsieck <jenso666@live.de>
2021-07-22 19:53:22 +0200

    Compile Regex

--- ChemSharp.Molecules/RegexUtil.cs
+++ ChemSharp.Molecules/RegexUtil.cs
@@ -15,9 +15,9 @@ namespace ChemSharp.Molecules
         /// </summary>
         private const string XYZPattern = @"([A-Z][a-z]{0,1}){1}\s*(-*\d*[,.]?\d*)\s*(-*\d*[,.]?\d*)\s*(-*\d*[,.]?\d*)";
 
-        public static readonly Regex AtomLabel = new(@"([A-Z][a-z]*)");
-        public static readonly Regex XYZString = new(XYZPattern);
-        public static readonly Regex SumFormula = new(SumFormulaPattern);
-        public static readonly Regex PointMatch = new("[.]");
+        public static readonly Regex AtomLabel = new(@"([A-Z][a-z]*)", RegexOptions.Compiled);
+        public static readonly Regex XYZString = new(XYZPattern, RegexOptions.Compiled);
+        public static readonly Regex SumFormula = new(SumFormulaPattern, RegexOptions.Compiled);
+        public static readonly Regex PointMatch = new("[.]", RegexOptions.Compiled);
     }
 }

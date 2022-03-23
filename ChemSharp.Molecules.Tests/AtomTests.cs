80984500ffbd2e6b3b126e156ae7aa5716d3a9a1
Jens Krumsieck <jenso666@live.de>
2022-03-23 22:08:22 +0100

    move test project

---
+++ ChemSharp.Molecules.Tests/AtomTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChemSharp.Molecules.Tests;
+
+[TestClass]
+public class AtomTests
+{
+    [TestMethod]
+    public void Atom_ShouldHaveSymbolAndLocation()
+    {
+        var atom = new Atom("H");
+        atom.Symbol.Should().Be("H");
+        atom.Location.Should().Be(Vector3.Zero);
+    }
+}

cbc2f16d9ddea20d85f19d942466e19a7916726b
Jens Krumsieck <jenso666@live.de>
2022-03-24 15:40:38 +0100

    cl

--- ChemSharp.Molecules/Molecule.cs
+++ ChemSharp.Molecules/Molecule.cs
@@ -85,6 +85,29 @@ public class Molecule : IExportable
 	[Obsolete("This field will be removed in 1.2.0")]
 	public IBondDataProvider BondDataProvider { get; private set; }
 
+	public int Electrons => Atoms.Sum(s => s.Electrons) - ImplicitCharge;
+
+	/// <summary>
+	///     Charge is made up of atoms charges and the implicit charge entered for the molecule
+	/// </summary>
+	public int Charge => InferredCharge + ImplicitCharge;
+
+	private int InferredCharge => Atoms.Sum(s => s.Charge);
+	public int ImplicitCharge { get; set; } = 0;
+
+	/// <summary>
+	///     Spin Multiplicity following the rules 2s+1
+	///     for s = 0 Multiplicity is 1 (default)
+	/// </summary>
+	public int Multiplicity { get; set; } = 1;
+
+	public double Spin => (Multiplicity - 1d) / 2d;
+
+	public bool IsParamagnetic => Electrons % 2 == 1;
+
+	public string SumFormula => Atoms.SumFormula();
+	public double MolecularWeight => Atoms.MolecularWeight();
+
 	/// <summary>
 	///     Recalculates Bonds based on Distances
 	///     WARNING! Discards every existing Bond information
@@ -165,27 +188,4 @@ public class Molecule : IExportable
 		Neighbors(a) != null && Neighbors(a).Any()
 			? Neighbors(a).Where(s => s != null && s.IsNonMetal)
 			: Enumerable.Empty<Atom>();
-
-	public int Electrons => Atoms.Sum(s => s.Electrons) - ImplicitCharge;
-
-	/// <summary>
-	///     Charge is made up of atoms charges and the implicit charge entered for the molecule
-	/// </summary>
-	public int Charge => InferredCharge + ImplicitCharge;
-
-	private int InferredCharge => Atoms.Sum(s => s.Charge);
-	public int ImplicitCharge { get; set; } = 0;
-
-	/// <summary>
-	///     Spin Multiplicity following the rules 2s+1
-	///     for s = 0 Multiplicity is 1 (default)
-	/// </summary>
-	public int Multiplicity { get; set; } = 1;
-
-	public double Spin => (Multiplicity - 1d) / 2d;
-
-	public bool IsParamagnetic => Electrons % 2 == 1;
-
-	public string SumFormula => Atoms.SumFormula();
-	public double MolecularWeight => Atoms.MolecularWeight();
 }

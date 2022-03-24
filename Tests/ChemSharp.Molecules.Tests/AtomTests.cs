56cc2af52e0d8c01ff0d98b80a357e573ef09e57
Jens Krumsieck <jenso666@live.de>
2022-03-24 09:47:54 +0100

    Add Equality Operators

--- Tests/ChemSharp.Molecules.Tests/AtomTests.cs
+++ Tests/ChemSharp.Molecules.Tests/AtomTests.cs
@@ -27,6 +27,20 @@ public class AtomTests
     }
 
     [Fact]
+    public void Atom_ShouldNotBeNull()
+    {
+        var atom1 = new Atom("He");
+        Assert.True(atom1 != null);
+    }
+    
+    [Fact]
+    public void Atom_ShouldBeNull()
+    {
+        Atom atom1 = null!;
+        Assert.True(atom1 == null);
+    }
+    
+    [Fact]
     public void Atoms_ShouldBeEqual_IfSymbolAndLocationMatch()
     {
         var atom1 = new Atom("He") { Location = new Vector3(1, 2, 3) };
@@ -49,23 +63,41 @@ public class AtomTests
         var atom2 = new Atom("He") { Location = new Vector3(1, 2, 3) };
         Assert.True(!atom1.Equals(atom2));
     }
+
+    [Fact]
+    public void Atoms_ShouldBeEqual_ViaOperator()
+    {
+        var atom1 = new Atom("He") { Location = new Vector3(1, 2, 3) };
+        var atom2 = new Atom("He") { Location = new Vector3(1, 2, 3) };
+        Assert.True(atom1 == atom2);
+    }
+
+    [Fact]
+    public void Atoms_ShouldNotBeEqual_IfLocationsDoNotMatch_ViaOperator()
+    {
+        var atom1 = new Atom("He") { Location = new Vector3(1, 2, 3) };
+        var atom2 = new Atom("He") { Location = new Vector3(1, 2, 4) };
+        Assert.True(atom1 != atom2);
+    }
+    
+    [Fact]
+    public void Atoms_ShouldNotBeEqual_IfSymbolsDoNotMatch_ViaOperator()
+    {
+        var atom1 = new Atom("He") { Location = new Vector3(1, 2, 3) };
+        var atom2 = new Atom("Ne") { Location = new Vector3(1, 2, 3) };
+        Assert.True(atom1 != atom2);
+    }
 }
 
 public class AtomTestDataGenerator : IEnumerable<object[]>
 {
-    readonly string atomSymbols =
-        "H He Li Be B C N O F Ne " +
-        "Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr " +
-        "Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe " +
-        "Cs Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn " +
-        "Fr Ra Ac Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og " +
-        "Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu " +
-        "Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr ";
-    private List<object[]> _data => atomSymbols
+    private const string AtomSymbols = "H He Li Be B C N O F Ne " + "Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr " + "Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe " + "Cs Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn " + "Fr Ra Ac Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og " + "Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu " + "Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr ";
+
+    private List<object[]> Data => AtomSymbols
         .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
         .Select(s => new object[] { s })
         .ToList();
-    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+    public IEnumerator<object[]> GetEnumerator() => Data.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }

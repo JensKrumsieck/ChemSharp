f0e19d96ab4926359649c1f07fad8d94d1cb9bdc
Jens Krumsieck <jenso666@live.de>
2022-03-26 18:49:07 +0100

    Add space

--- Tests/ChemSharp.Benchmarks/ConnectedFiguresBenchmarks.cs
+++ Tests/ChemSharp.Benchmarks/ConnectedFiguresBenchmarks.cs
@@ -9,21 +9,22 @@ namespace ChemSharp.Benchmarks;
 public class ConnectedFiguresBenchmarks
 {
 	private readonly UndirectedGraph<int, Edge<int>> _graph;
+
 	public ConnectedFiguresBenchmarks()
 	{
 		var vertices = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		var edges = new[]
 		{
-			new Edge<int>(1, 3), new Edge<int>(2, 4), new Edge<int>(3, 5), new Edge<int>(3, 6),
-			new Edge<int>(4, 7), new Edge<int>(5, 8), new Edge<int>(5, 6), new Edge<int>(8, 9),
-			new Edge<int>(9, 10),
+			new Edge<int>(1, 3), new Edge<int>(2, 4), new Edge<int>(3, 5), new Edge<int>(3, 6), new Edge<int>(4, 7),
+			new Edge<int>(5, 8), new Edge<int>(5, 6), new Edge<int>(8, 9), new Edge<int>(9, 10),
 		};
 		_graph = new UndirectedGraph<int, Edge<int>>(vertices, edges);
 	}
+
 	[Benchmark(Baseline = true)]
 	public void OldConnectedFigures()
 	{
-		var result =DFSUtil.ConnectedFigures(_graph.Vertices, v => _graph.Neighbors(v));
+		var result = DFSUtil.ConnectedFigures(_graph.Vertices, v => _graph.Neighbors(v));
 	}
 
 	[Benchmark]

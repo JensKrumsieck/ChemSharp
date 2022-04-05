09b34aa97cdb7265c83418258faff108074b892e
Jens Krumsieck <jenso666@live.de>
2022-04-05 13:48:39 +0200

    Mol2Benchmarks.cs

---
+++ Tests/ChemSharp.Benchmarks/Mol2Benchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using ChemSharp.Molecules;
+using ChemSharp.Molecules.DataProviders;
+using ChemSharp.Molecules.Formats;
+
+namespace ChemSharp.Benchmarks;
+
+[MemoryDiagnoser, MarkdownExporter]
+public class Mol2Benchmarks
+{
+	[Params("files/myo.mol2", "files/benzene.mol2")]
+	public string mol2File { get; set; }
+
+	[Benchmark]
+	public void DataProviderMethodMol2()
+	{
+		var mol = new Molecule(new Mol2DataProvider(mol2File));
+	}
+
+	[Benchmark(Baseline = true)]
+	public void FormatMethodMol2()
+	{
+		var mol = Mol2Format.Read(mol2File);
+	}
+}

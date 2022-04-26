27da41127d940d9c73a96150bbe8bcd8e3d5f8d1
Jens Krumsieck <jenso666@live.de>
2022-04-26 15:09:25 +0200

    Add MolFormat.cs

---
+++ Tests/ChemSharp.Molecules.Tests/Formats/MolFormatTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using ChemSharp.Molecules.Formats;
+using FluentAssertions;
+using Xunit;
+
+namespace ChemSharp.Molecules.Tests.Formats;
+
+public class MolFormatTests
+{
+	[Theory, InlineData("files/benzene_3d.mol", 12, 12),
+	 InlineData("files/benzene_arom.mol", 12, 12),
+	 InlineData("files/benzene.mol", 6, 6)]
+	public void MolFormat_CanReadPlausibleData(string file, int atomsCount, int bondsCount)
+	{
+		var mol = MolFormat.Read(file);
+		mol.Atoms.Count.Should().Be(atomsCount);
+		mol.Atoms.Count(s => s.Symbol == "DA").Should().Be(0);
+		mol.Bonds.Count.Should().Be(bondsCount);
+	}
+}

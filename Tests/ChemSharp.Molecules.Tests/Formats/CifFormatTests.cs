935d1c52e3680c55d2704ec05a0aeb3c13595e55
Jens Krumsieck <jenso666@live.de>
2022-04-26 10:45:17 +0200

    Add mmcif file

--- Tests/ChemSharp.Molecules.Tests/Formats/CifFormatTests.cs
+++ Tests/ChemSharp.Molecules.Tests/Formats/CifFormatTests.cs
@@ -11,7 +11,8 @@ public class CifFormatTests
 	 InlineData("files/cif_noTrim.cif", 79, 89),
 	 InlineData("files/mmcif.cif", 1291, 1251),
 	 InlineData("files/ligand.cif", 44, 46),
-	 InlineData("files/147288.cif", 206, 230)]
+	 InlineData("files/147288.cif", 206, 230),
+	 InlineData("files/4r21.cif", 6752, 6892)]
 	public void CifFormat_CanReadPlausibleData(string file, int atomsCount, int bondsCount)
 	{
 		var mol = CifFormat.Read(file);

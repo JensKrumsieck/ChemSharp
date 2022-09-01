460c887fdc0f59ba6e845245040a06d3b5a549ef
Jens Krumsieck <jenso666@live.de>
2022-09-01 17:12:43 +0200

    Add Tests for conversion proj

---
+++ Tests/ChemSharp.UnitConversion.Tests/MagneticConversionTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace ChemSharp.UnitConversion.Tests;
+
+public class MagneticConversionTests
+{
+	[Fact]
+	public void Gauss_To_Millitesla()
+	{
+		var converter = new MagneticUnitConverter("G", "mT");
+		Assert.Equal(.1, converter.Convert(1));
+		Assert.Equal(10, converter.ConvertInverted(1));
+	}
+}
---
+++ Tests/ChemSharp.UnitConversion.Tests/MassConversionTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ChemSharp.UnitConversion.Tests;
+
+public class MassConversionTests
+{
+	[Fact]
+	public void SolarMass_To_EarthMass()
+	{
+		var converter = new MassUnitConverter("Solar Mass", "Earth Mass");
+		converter.Convert(1).Should().BeApproximately(332946.0487, 1.5);
+	}
+
+	[Fact]
+	public void Lbs_to_g()
+	{
+		var converter = new MassUnitConverter("lbs", "g");
+		converter.Convert(1).Should().Be(453.59237);
+	}
+}

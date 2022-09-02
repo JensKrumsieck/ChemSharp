cf1e646a9e16161b10e80db84b0d91427772a5cf
Jens Krumsieck <jenso666@live.de>
2022-09-02 22:17:59 +0200

    Add To-Methods

--- ChemSharp.Molecules/Extensions/MoleculeLinq.cs
+++ ChemSharp.Molecules/Extensions/MoleculeLinq.cs
@@ -16,4 +16,29 @@ public static class MoleculeLinq
 		var bonds = mol.Bonds.Where(b => predicate(b.Atom1) && predicate(b.Atom2));
 		return new Molecule(atoms, bonds);
 	}
+
+	/// <summary>
+	/// Use Constructor for Chaining!
+	/// </summary>
+	/// <param name="atoms"></param>
+	/// <returns></returns>
+	public static Molecule ToMolecule(this IEnumerable<Atom> atoms) => new(atoms);
+
+	/// <summary>
+	/// Create Molecule from Bonds
+	/// </summary>
+	/// <param name="bonds"></param>
+	/// <returns></returns>
+	public static Molecule ToMolecule(this IEnumerable<Bond> bonds)
+	{
+		var atoms = new List<Atom>();
+		var enumerate = bonds.ToList();
+		foreach (var bond in enumerate)
+		{
+			if (!atoms.Contains(bond.Atom1)) atoms.Add(bond.Atom1);
+			if (!atoms.Contains(bond.Atom2)) atoms.Add(bond.Atom2);
+		}
+
+		return new Molecule(atoms, enumerate);
+	}
 }

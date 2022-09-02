1aa5de949b7e917dfc1b83a8bee2e2763ee66625
Jens Krumsieck <jenso666@live.de>
2022-09-02 22:10:49 +0200

    add MoleculeLinq.cs

---
+++ ChemSharp.Molecules/Extensions/MoleculeLinq.cs
@@ -0,0 +1,19 @@
+﻿namespace ChemSharp.Molecules.Extensions;
+
+public static class MoleculeLinq
+{
+	/// <summary>
+	/// Returns a subset of a molecule matching the predicate foreach atom.
+	/// </summary>
+	/// <param name="mol"></param>
+	/// <param name="predicate"></param>
+	/// <returns></returns>
+	public static Molecule Where(this Molecule mol, Func<Atom, bool> predicate)
+	{
+		//get subset of atoms
+		var atoms = mol.Atoms.Where(predicate);
+		//get all bonds where both atoms are matching the predicate
+		var bonds = mol.Bonds.Where(b => predicate(b.Atom1) && predicate(b.Atom2));
+		return new Molecule(atoms, bonds);
+	}
+}
---
+++ Tests/ChemSharp.Molecules.Tests/Extensions/MoleculeLinqTests.cs
@@ -0,0 +1,20 @@
+﻿using ChemSharp.Molecules.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace ChemSharp.Molecules.Tests.Extensions;
+
+public class MoleculeLinqTests
+{
+	[Fact]
+	public void Where_Is_Valid()
+	{
+		var molecule = Molecule.FromFile("files/corrole.mol");
+
+		molecule.Where(a => a.IsMetal).Atoms.Should().BeEmpty();
+		molecule.Where(a => a.IsMetal).Bonds.Should().BeEmpty();
+
+		molecule.Where(a => a.Symbol != "H").Atoms.Should().HaveCount(23);
+		molecule.Where(a => a.Symbol != "H").Bonds.Should().HaveCount(27);
+	}
+}

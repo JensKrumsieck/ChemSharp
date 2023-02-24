dc30980e66d318f02e0d825465def04ed3fe1e98
Jens Krumsieck <jenso666@live.de>
2023-02-24 14:20:33 +0100

    Remove obsolete files

--- ChemSharp/DataProviders/AbstractXYDataProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ChemSharp.DataProviders;
-
-public abstract class AbstractXYDataProvider : AbstractDataProvider, IXYDataProvider
-{
-	protected AbstractXYDataProvider(string path) : base(path) { }
-	public DataPoint[] XYData { get; set; }
-}
